06727a9bfe803c815be79e25ff101c5f90a13dc3
AlinaKlochai <mrs.ali@tuta.io>
2024-05-28 14:16:28 +0200

    =

--- 03_react_iprops_state_27_05_2024/Task.js
+++ 03_react_iprops_state_27_05_2024/Task.js
@@ -3,18 +3,27 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 const Task = () => {
     //const isEdit = false;
     const [isEdit, setIsEdit] = React.useState(false);   //возвращает [isEdit, setIsEdit] array(2)
+
+    const [updatedTask, setUpdatedTask] = React.useState('Task1');
     
+    const textId = React.useRef();
+
+    const handleClickSave = () => {
+        setUpdatedTask(textId.current.value);
+        setIsEdit(false)
+    }
+
     if(isEdit){
         return(
             <>
-                <textarea defaultValue=''></textarea>
-                <button onClick={() =>  setIsEdit(false)}>Save</button>
+                <textarea ref={textId} defaultValue={updatedTask}></textarea>
+                <button onClick={handleClickSave}>Save</button>
             </>
         )
     } else{
         return(
             <>
-                <p>Name</p>
+                <p>{updatedTask}</p>
                 <button onClick={() => setIsEdit(true)}>Edit</button>
                 <button>Delete</button>
             </>
