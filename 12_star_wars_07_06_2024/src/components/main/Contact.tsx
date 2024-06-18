4e252bbf690d96657e0fd6a393020894e6e03428
AlinaKlochai <mrs.ali@tuta.io>
2024-06-18 11:12:18 +0200

    library axios

--- 12_star_wars_07_06_2024/src/components/main/Contact.tsx
+++ 12_star_wars_07_06_2024/src/components/main/Contact.tsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+interface IPlanet {
+  name: string
+};
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const [planets, setPlanets] = useState<string[]>([]);
 
-export default Contact
+  useEffect(() => {
+    const fetchPlanets = async () => {
+      try {
+        // const response = await fetch('https://sw-info-api.herokuapp.com/v1/planets');
+        // const data = await response.json();
+        // return data
+
+        //with axios
+        const data = (await axios.get<IPlanet[]>('https://sw-info-api.herokuapp.com/v1/planets')).data;
+        setPlanets(data.map(e => e.name));
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchPlanets();
+  }, [])
+
+  return (
+    <form style={{ display: 'flex', flexDirection: 'column' }}>
+      <label>
+        Firstname:
+        <input type="text" placeholder='FirstName...' />
+      </label>
+      <label >
+        Lastname:
+        <input type="text" placeholder='LastName...' />
+      </label>
+      <label>Planets:
+        <select required>     //reguired- обязательное поле для заполнения
+          <option value="">Choose your planet:</option>
+          {planets.map(planetName => (
+            <option key={planetName} value={planetName}>{planetName}</option>
+          ))}
+        </select>
+      </label>
+      <label>Comment:
+        <textarea name="" id="" cols={30} rows={10}></textarea>
+      </label>
+      <label >
+        Gender:
+        <label>Male <input type="radio" value='male' name='gender'/></label>
+        <label>Female <input type="radio" value='female' name='gender' /></label>
+        <label>Other <input type="radio" value='other' name='gender' /></label>
+      </label>
+      <label>Confirm:
+        <input type='checkbox' />
+        I agree...
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Contact;
